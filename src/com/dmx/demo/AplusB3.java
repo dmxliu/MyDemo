2588aebb0ee031900cb5d986d4284ec2c97ac1c6
asialiu <asialiu@ubuntu>
2015-01-08 22:57:39 +0800

    a+b

--- src/com/dmx/demo/AplusB3.java
+++ src/com/dmx/demo/AplusB3.java
@@ -7,6 +7,8 @@
  */
 package com.dmx.demo;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -28,14 +30,39 @@ public class AplusB3 {
 	 * @param args
 	 */
 	public static void main( String [] args ) {
+		Map<String, Integer> map = new HashMap<String , Integer>();
+		map.put( "one", 1 );
+		map.put( "two", 2 );
+		map.put( "three", 3 );
+		map.put( "four", 4 );
+		map.put( "five", 5 );
+		map.put( "six", 6 );
+		map.put( "seven", 7 );
+		map.put( "eight", 8 );
+		map.put( "nine", 9 );
+		map.put( "zero", 0 );
+		
+		
 		Scanner cin = new Scanner(System.in);
+		String a =""; 
+		String result =""; 
 		 while(cin.hasNext()){
-			 
-			 String a = cin.next();
-			 long ai = Long.parseLong( a.replaceAll( ",", "") );
-			 String b = cin.next();
-			 long bi = Long.parseLong( b.replaceAll( ",", "") );
-			 System.out.println(ai+bi);
+			 String str = cin.next();
+			 if(str.equals( "+" )){
+				 result = a ;
+				 a ="";
+			 }else if(str.equals( "=" )){
+				 int ai = Integer.parseInt( result );
+				 int bi = Integer.parseInt( a );
+				 if(result.equals( "0" )&&a.equals( "0" )){
+					 break;
+				 }
+				 System.out.println(ai+bi);
+				 a = "";
+				 result="";
+			 }else{
+				 a=a+map.get( str );
+			 }
 		 }
 
 	}
